eb4a10b1ed481b9969d77b34689d3993361f6b95
sooshe <sooshe@sooshebot.com>
2019-09-23 15:17:49 -0500

    added data,renamed home and article content pages

--- how-to/src/components/landing-page.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-import { Route, NavLink } from 'react-router-dom';
-
-
---
+++ how-to/src/data.js
@@ -0,0 +1,38 @@
+export default [
+    {
+        title: How to Take a Picture
+        id: 0,
+        imageUrl:https://unsplash.com/photos/7v_lSDRaOO0
+        summary: 'There are a few simple tricks to taking a good picture; light, composition, and ...'
+    },
+    {
+        title: 
+        id: 1,
+        imageUrl:
+        summary:
+    },
+    {
+        title:
+        id: 2,
+        imageUrl:
+        summary:
+    },
+    {
+        title:
+        id: 3,
+        imageUrl:
+        summary:
+    },
+    {
+        title:
+        id: 4,
+        imageUrl:
+        summary:
+    },
+    {
+        title:
+        id: 5,
+        imageUrl:
+        summary:
+    }
+]
