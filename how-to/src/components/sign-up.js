6012bd1c8b2cd37d5e53844a0fd1e0e94e17f3f2
amxra <amiradediran@gmail.com>
2019-09-23 20:10:29 +0100

    Install formik and create form component

---
+++ how-to/src/components/sign-up.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+
+function SignUp(){
+
+
+    return(
+        <Formik>
+            
+        </Formik>
+    )
+
+
+}
