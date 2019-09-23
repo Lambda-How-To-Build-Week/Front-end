ef0eb7859b25d37e5a355a522e604e695d1083f1
sooshe <sooshe@sooshebot.com>
2019-09-23 18:07:10 -0500

    edits to home.js

--- how-to/src/components/home.js
+++ how-to/src/components/home.js
@@ -1,9 +1,25 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Link } from 'react-router-dom';
 import data from '../data';
+import SignUp from "./sign-up";
 import "./styles.css";
 
-function Home() {
-
+function Home(props) {
+    return (
+        <div className='articles-wrapper'>
+           props.data.map(articles => (
+               <div className='articles-card'>
+                   <Link to="/sign-up">Sign In
+                   <img className='articles-image'
+                   src={articles-imageUrl}
+                   alt={articles.name}/>
+                   <p>{articles.name}</p>
+                   </Link>
+                   <p>{articles.description}</p>
+               </div>
+           ))}
+        </div>
+    );
 }
 
 
