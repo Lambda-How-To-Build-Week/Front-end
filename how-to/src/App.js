4fc83a2f70f8f8b6733b9b9014f03d8726d8c61d
sooshe <sooshe@sooshebot.com>
2019-09-24 12:51:56 -0500

    added home button to app.js, modified index.js to include Router

--- how-to/src/App.js
+++ how-to/src/App.js
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Route, Link } from 'react-router-dom';
 // import logo from './logo.svg';
 import './App.css';
 import SignUp from "./components/sign-up";
 
+import home from './components/home';
+import data from './data';
+
 function App() {
+  const [items] = useState(data);
   return (
     <div className="App">
+      <nav>
+        <h1 className='app-header'></h1>
+        <div className='nav-links'>
+          <Link to='/'>Home</Link>
+        </div>
+      </nav>
+      <Route exact path='/' render={props => ( <home {...props} articles={items}/>)} />
      <SignUp/>
     </div>
   );
--- how-to/src/components/home.js
+++ how-to/src/components/home.js
@@ -1,26 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import data from '../data';
-import SignUp from "./sign-up";
-import "./styles.css";
+// import "./styles.css";
 
 function Home(props) {
     return (
         <div className='articles-wrapper'>
-           props.data.map(articles => (
-               <div className='articles-card'>
-                   <Link to="/sign-up">Sign In
-                   <img className='articles-image'
-                   src={articles-imageUrl}
-                   alt={articles.name}/>
-                   <p>{articles.name}</p>
+           props.data.map(article => (
+               <div className='article-card' key={data.id}>
+                   <Link to={`/${data.id}`}>
+                   <img className='article-list-image'
+                   src={data.imageUrl}
+                   alt={data.name}/>
+                   <p>{data.name}</p>
                    </Link>
-                   <p>{articles.description}</p>
+                   <p>{data.description}</p>
                </div>
            ))}
         </div>
     );
 }
-
 
 export default Home;
